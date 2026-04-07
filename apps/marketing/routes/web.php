d597aa7e18880346d1a75490c520c600e02bf978
agadirconcept2019 <agadir.concept2019@gmail.com>
2026-04-07 15:09:29 +0100

    Add files via upload

--- apps/marketing/routes/web.php
+++ apps/marketing/routes/web.php
@@ -5,6 +5,19 @@ declare(strict_types=1);
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\MediaController;
 use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\PageController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\ReusableBlockController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\PublicSite\PageController as PublicPageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PublicPageController::class, 'home'])->name('public.home');
+Route::get('/services', [PublicPageController::class, 'services'])->name('public.services');
+Route::get('/services/{slug}', [PublicPageController::class, 'serviceDetail'])->name('public.services.show');
+Route::get('/about', [PublicPageController::class, 'about'])->name('public.about');
+Route::get('/contact-quote', [PublicPageController::class, 'contactQuote'])->name('public.contact-quote');
+Route::get('/faq', [PublicPageController::class, 'faq'])->name('public.faq');
 
 Route::prefix('admin')->name('admin.')->group(function (): void {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login.form');
@@ -15,6 +28,8 @@ Route::prefix('admin')->name('admin.')->group(function (): void {
 
         Route::view('/', 'admin.dashboard')->name('dashboard');
 
+        Route::resource('pages', PageController::class)->except(['show']);
+        Route::resource('posts', PostController::class)->except(['show']);
         Route::resource('menus', MenuController::class)->except(['show']);
 
         Route::get('media', [MediaController::class, 'index'])->name('media.index');
@@ -26,6 +41,8 @@ Route::prefix('admin')->name('admin.')->group(function (): void {
         Route::post('settings', [SettingController::class, 'store'])->name('settings.store');
         Route::delete('settings/{setting}', [SettingController::class, 'destroy'])->name('settings.destroy');
 
-        Route::resource('blocks', ReusableBlockController::class)->parameters(['blocks' => 'block'])->except(['show']);
+        Route::resource('blocks', ReusableBlockController::class)
+            ->parameters(['blocks' => 'block'])
+            ->except(['show']);
     });
 });
