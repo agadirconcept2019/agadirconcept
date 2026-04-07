d14b166f497986eb4fea25e7eba6cfddaf8b03d8
agadirconcept2019 <agadir.concept2019@gmail.com>
2026-04-07 15:07:44 +0100

    Add files via upload

--- apps/marketing/bootstrap/app.php
+++ apps/marketing/bootstrap/app.php
@@ -2,7 +2,7 @@
 
 use App\Http\Middleware\AdminSessionAuth;
 use Illuminate\Foundation\Application;
-
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -22,5 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin.auth' => AdminSessionAuth::class,
         ]);
     })
-
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })
     ->create();
